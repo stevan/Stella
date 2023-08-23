19572568c37c2b979575885db0134a5e3560589d
Stevan Little <stevan.little@gmail.com>
2023-08-24 01:00:06 +0200

    some notes

--- lib/Stella/Behavior/Method.pm
+++ lib/Stella/Behavior/Method.pm
@@ -22,6 +22,18 @@ class Stella::Behavior::Method :isa(Stella::Behavior) {
 
         exists $_method_cache{ $symbol } || confess "Unsupported message ($symbol)";
 
+        # FIXME:
+        # This is not exactly how I want it to work,
+
+        # perl -E 'package Bar { sub foo { "FOO" } }; package Foo { sub bar { "BAR" } }; say Foo->can(*Bar::foo)->()'
+        # FOO
+
+        # basically `can` will always return true and will just
+        # call the method with the invocant and hope it works.
+        #
+        # this will most certainly break with class objects as
+        # they will not have the same instance pad.
+
         my $method = $_method_cache{ $symbol } //= $actor->can($symbol);
 
         $actor->$method( $ctx, $message );
